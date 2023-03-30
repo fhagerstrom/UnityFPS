776719f8975b3e0e8874639c997bc568fa23c650
Fabian Hagerström <fabianhagerstrom@hotmail.com>
2023-03-30 22:52:42 +0200

    Added: New weapon + weapon switch
    Added a sniper rifle with its own properties and added the ability to switch between the weapons.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     [SerializeField] private GunData gunData;
-    [SerializeField] private Transform muzzle;
+    [SerializeField] private Transform cam;
 
     float timeSinceLastShot;
 
@@ -16,9 +16,11 @@ public class Gun : MonoBehaviour
         PlayerShoot.reloadInput += Reloading; 
     }
 
+    private void OnDisable() => gunData.reloading = false;
+
     public void Reloading()
     {
-        if(!gunData.reloading) 
+        if(!gunData.reloading && this.gameObject.activeSelf) 
         {
             // Reload
             StartCoroutine(Reload());
@@ -44,7 +46,7 @@ public class Gun : MonoBehaviour
         {
             if(CanShoot()) 
             {
-                if(Physics.Raycast(muzzle.position, transform.forward, out RaycastHit hitInfo, gunData.maxDistance)) 
+                if(Physics.Raycast(cam.position, cam.forward, out RaycastHit hitInfo, gunData.maxDistance)) 
                 {
                     IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();
                     damageable?.TakeDamage(gunData.damage);
@@ -62,7 +64,7 @@ public class Gun : MonoBehaviour
     private void Update()
     {
         timeSinceLastShot += Time.deltaTime;
-        Debug.DrawRay(muzzle.position, muzzle.forward);
+        Debug.DrawRay(cam.position, cam.forward * gunData.maxDistance);
     }
 
     private void OnGunShoot()
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Target : MonoBehaviour, IDamageable
 {
 
     private float health = 100.0f;
+    // private float respawnTimer = 2.0f;
     public void TakeDamage(float damage)
     {
         health -= damage;
         if(health <= 0) 
         {
-            Destroy(gameObject);
+            gameObject.SetActive(false);
         }
     }
 }
